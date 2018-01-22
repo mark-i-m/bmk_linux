21577767fe4053aa9f80428680a2aa20af35b81d
Mark Mansi <markm@cs.wisc.edu>
2018-01-22 14:17:07 -0600

    typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! A bunch of utilities I've collected over time that are useful for measuring stuff on Linux.
 //!
-//! I've used these on Linux 4.4, but many should also work on older and new versions. WMMV. Check
+//! I've used these on Linux 4.4, but many should also work on older and new versions. YMMV. Check
 //! the Linux docs.
 
 pub mod procfs;
